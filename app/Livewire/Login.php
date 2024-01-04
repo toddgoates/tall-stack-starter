61882397dca978510c7b325a855dcc16ce23251f
Todd Goates <toddgoates@gmail.com>
2024-01-03 23:22:40 -0700

    Add Tailwind CSS configuration files, update login page layout, and add login functionality.

--- app/Livewire/Login.php
+++ app/Livewire/Login.php
@@ -2,12 +2,33 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 #[Title('Sign In')]
 class Login extends Component
 {
+    #[Validate('required|email')]
+    public $email;
+
+    #[Validate('required|min:8')]
+    public $password;
+
+    public function login()
+    {
+        $credentials = $this->validate();
+
+        if (Auth::attempt($credentials)) {
+            session()->regenerate();
+ 
+            return redirect()->route('home');
+        }
+
+        $this->addError('email', 'Your credentials are invalid.');
+    }
+    
     public function render()
     {
         return view('livewire.login');
--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,3 +1,22 @@
-<div>
-    This is a login page
-</div>
+<main class="h-screen bg-gray-300 text-gray-800 grid justify-center items-center">
+    <div class="bg-gray-100 rounded p-6 shadow-lg">
+        <h1 class="text-4xl font-extrabold mb-8">Sign in to your account</h1>
+        <form class="space-y-8" wire:submit="login">
+            <div>
+                <label for="email" class="block mb-1 font-semibold">Email</label>
+                <input type="email" wire:model="email" id="email" class="w-full rounded" />
+                @error('email') <span class="text-red-500">{{ $message }}</span> @enderror
+            </div>
+
+            <div>
+                <label for="password" class="block mb-1 font-semibold">Password</label>
+                <input type="password" wire:model="password" id="password" class="w-full rounded" />
+                @error('password') <span class="text-red-500">{{ $message }}</span> @enderror
+            </div>
+
+            <div>
+                <button type="submit" class="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded py-2">Sign in</button>
+            </div>
+        </form>
+    </div>
+</main>
